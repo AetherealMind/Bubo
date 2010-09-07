ba60a65cac8c7f947378d3bc0be758fe2b4be2e0
Ramon Brooker <cognition.mind@gmail.com>
2010-09-07 16:13:40 -0400

    fixing the iterator script

--- iterate-item.rb
+++ iterate-item.rb
@@ -1,11 +1,15 @@
 #!/usr/bin/ruby
 
-20.each do |n|
-  `mongoimport -db opencycdb #{'properties/prop + " " + #{n} + .json'} -c properties --jsonArray --upsert`
+#figure out what I'm not seeing with the concatination of the 'n' variable for the file name
+
+
+(1..20).each do |n|
+  `mongoimport -db opencycdb -c properties --jsonArray --upsert #{'properties/prop' << " " << '#{n}' << '.json'}`
 end
-20.each do |n|
-  `mongoimport -db opencycdb #{'types/type + #{n} + .json'} -c types --jsonArray --upsert`
+(1..20).each do |n|
+  `mongoimport -db opencycdb #{'types/type << #{n} << .json'} -c types --jsonArray --upsert`
 end
-32.each do |n|
-  `mongoimport -db opencycdb #{'item + #{n} + .json'} -c item --jsonArray --upsert`
+
+(1..32).each do |n|
+  `mongoimport -db opencycdb #{'item << #{n} << .json'} -c item --jsonArray --upsert`
 end
